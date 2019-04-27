05a18f02e95c38f2ac3650dafaff5b0236063cd7
Samuel Stenton <sthstenton@gmail.com>
2019-04-27 14:36:04 +0100

    create method

--- handler.js
+++ handler.js
@@ -9,12 +9,25 @@ const deployements = client.apis.apps.v1.namespaces('default').deployments;
 const services = client.api.v1.namespaces('default').services;
 const ingresses = client.apis.extensions.v1beta1.namespaces('default').ingresses;
 
-const create = (event) => success()
+const create = async (event, context, callback) => {
+  // name
+  // image
+  // port 
+  const body = JSON.parse(event.body);
+
+  const deployment = deployements.post({ body: { "apiVersion": "apps/v1", "kind": "Deployment", "metadata": { "name": `${name}-deployment`, "labels": { "app": name } }, "spec": { "replicas": 1, "selector": { "matchLabels": { "app": body.name } }, "template": { "metadata": { "labels": { "app": body.name } }, "spec": { "containers": [{ "name": body.name, "image": body.image, "ports": [{ "containerPort": body.port }] }] } } } } })
+  const service = services.post({ body: { "apiVersion": "v1", "kind": "Service", "metadata": { "name": `${body.name}-service`, "namespace": "default" }, "spec": { "selector": { "app": body.name }, "type": "ClusterIP", "ports": [{ "protocol": "TCP", "port": body.port }] } } })
+  const ingress = ingresses.post({ body: { "apiVersion": "extensions/v1beta1", "kind": "Ingress", "metadata": { "name": `${body.name}-ingress`, "namespace": "default", "annotations": { "kubernetes.io/ingress.class": "traefik" } }, "spec": { "rules": [{ "host": `${body.name}.samuelstenton.com`, "http": { "paths": [{ "backend": { "serviceName": `${body.name}-service`, "servicePort": body.port } }] } }] } } });
+
+  await Promise.all([deployment, service, ingress]).catch(console.log)
+  return success(callback);
+}
 const update = (event) => success()
 const destroy = (event) => success()
+
 const list = async (event, context, callback) => {
   const { body } = await ingresses.get()
-  
+
   return success(callback, body.items);
 }
 
