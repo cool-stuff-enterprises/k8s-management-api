e6b7356426885e322ffddfc7de902585c1a26367
Samuel Stenton <sthstenton@gmail.com>
2019-04-27 13:38:49 +0100

    output content

--- handler.js
+++ handler.js
@@ -5,21 +5,30 @@ const Client = require('kubernetes-client').Client
 const config = K8sConfig.fromKubeconfig('./config');
 
 const client = new Client({ config, version: '1.9' })
+const deployements = client.apis.apps.v1.namespaces('default').deployments;
+const services = client.api.v1.namespaces('default').services;
+const ingresses = client.apis.extensions.v1beta1.namespaces('default').ingresses;
 
 const create = (event) => success()
 const update = (event) => success()
 const destroy = (event) => success()
-const list = (event) => success()
+const list = (event, context, callback) => {
+  ingresses.get().then(res => {
+    console.log(res);
+  })
+
+  return success(callback);
+}
 
 /**
  * Return a 
  * @param {String} message 
  */
-const success = (message = 'Success') => ({
+const success = (callback, message = 'Success') => (callback(null, {
   statusCode: 200,
     body: JSON.stringify({
       message: message
     }),
-  });
+  }));
 
 module.exports = { create, destroy, update, list }
